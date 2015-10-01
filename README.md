65d97270ee302331380bbcee7e01de6ff90f6b14
ziguzagu <ziguzagu@gmail.com>
2015-10-01 10:10:04 +0900

    Add more documents

--- README.md
+++ README.md
@@ -20,6 +20,20 @@ Mojolicious::Plugin::ClientIP - Get client's IP address from X-Forwarded-For
 
 Mojolicious::Plugin::ClientIP is a Mojolicious plugin to get an IP address looks like client, not proxy, from X-Forwarded-For header.
 
+# METHODS
+
+## client\_ip
+
+Find a client IP address from X-Forwarded-For. Private network addresses in XFF are ignored by default. If the good IP address is not found, it returns Mojo::Transaction#remote\_address.
+
+# OPTIONS
+
+## ignore
+
+Specify IP list to be ignored with ArrayRef.
+
+    plugin 'ClientIP', ignore => [qw(192.0.2.1 192.0.2.16/28)];
+
 # LICENSE
 
 Copyright (C) Six Apart, Ltd. <sixapart@cpan.org>
--- lib/Mojolicious/Plugin/ClientIP.pm
+++ lib/Mojolicious/Plugin/ClientIP.pm
@@ -99,6 +99,20 @@ Mojolicious::Plugin::ClientIP - Get client's IP address from X-Forwarded-For
 
 Mojolicious::Plugin::ClientIP is a Mojolicious plugin to get an IP address looks like client, not proxy, from X-Forwarded-For header.
 
+=head1 METHODS
+
+=head2 client_ip
+
+Find a client IP address from X-Forwarded-For. Private network addresses in XFF are ignored by default. If the good IP address is not found, it returns Mojo::Transaction#remote_address.
+
+=head1 OPTIONS
+
+=head2 ignore
+
+Specify IP list to be ignored with ArrayRef.
+
+    plugin 'ClientIP', ignore => [qw(192.0.2.1 192.0.2.16/28)];
+
 =head1 LICENSE
 
 Copyright (C) Six Apart, Ltd. E<lt>sixapart@cpan.orgE<gt>
